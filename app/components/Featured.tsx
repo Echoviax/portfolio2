6372d99bedd6573d6732049d08f33ceb06c6bd36
Echoviax <echoviax@gmail.com>
2026-06-11 14:21:59 -0400

    emolb

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -7,14 +7,15 @@ import ProjectPopup from "./ProjectPopup";
 
 const emmolb: Project = {
     id: 1,
-    title: "Bayseball",
+    title: "EMMOLB",
     description: "Lorem ipsum dolor sit amet",
     links: [
-        {href: "https://github.com/echoviax/bayseball-ui", text: "Github", newTab: true}
+        {href: "https://github.com/echoviax/emmolb", text: "GitHub", newTab: true},
+        {href: "https://emmolb.com", text: "Live", newTab: true}
     ],
-    categories: ['webdes', 'webdev', 'gamedes', 'gamedev'],
-    dates: "June 2025 - August 2025",
-    imagePath: "bayseball"
+    categories: ['webdes', 'webdev'],
+    dates: "June 2025 - Current",
+    imagePath: "emmolb"
 }
 
 export default function FeaturedPage() {
